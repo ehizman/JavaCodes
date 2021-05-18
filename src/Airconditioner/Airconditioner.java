fc2be9dadf3169d357ea2c7589d51dae10b0b9c1
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-18 13:48:47 +0100

    added functionality| temperature can be set

--- src/Airconditioner/Airconditioner.java
+++ src/Airconditioner/Airconditioner.java
@@ -3,6 +3,7 @@ package Airconditioner;
 public class Airconditioner {
     private String productName;
     private boolean isOn;
+    private int temperature;
 
     public Airconditioner(String productName) {
         this.productName = productName;
@@ -13,10 +14,19 @@ public class Airconditioner {
     }
 
     public void setOn(boolean condition) {
+        setTemperature(16);
         isOn = condition;
     }
 
     public boolean isOn() {
         return isOn;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
     }
 }
--- test/Airconditioner/Airconditioner_Test.java
+++ test/Airconditioner/Airconditioner_Test.java
@@ -37,4 +37,28 @@ public class Airconditioner_Test {
         //assert
         assertFalse(newAC.isOn());
     }
+
+    @Test
+    void thatAcIsSetOnTo16DegreesWhenPoweredOnForTheFirstTime(){
+        //given
+        Airconditioner newAC;
+        //when
+        newAC = new Airconditioner("iCool By Ehizman");
+        newAC.setOn(true);
+        //assert
+        assertEquals(16, newAC.getTemperature());
+    }
+
+    @Test
+    void testThatTemperatureCanBeSet(){
+        //given
+        Airconditioner newAC;
+        //when
+        newAC = new Airconditioner("iCool By Ehizman");
+        newAC.setOn(true);
+        newAC.setTemperature(20);
+        //assert
+        assertEquals(20, newAC.getTemperature());
+
+    }
 }
