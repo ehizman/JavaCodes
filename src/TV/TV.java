60d6bd51e7e3776a163d29188022c2d29608c501
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-18 17:44:10 +0100

    added new functionality to set TV channel

--- src/TV/TV.java
+++ src/TV/TV.java
@@ -3,6 +3,11 @@ package TV;
 public class TV {
     private final String productName = "Ehizman";
     private boolean isOn;
+    private int channel;
+
+    public TV(int initialVolume, int initialChannel) {
+        channel = initialChannel;
+    }
 
     String getName() {
         return productName;
@@ -14,5 +19,21 @@ public class TV {
 
     public boolean isOn() {
         return isOn;
+    }
+
+    public void moveToNextChannel() {
+        channel++;
+    }
+
+    public int getCurrentChannel() {
+        return channel;
+    }
+
+    public int getCurrentVolume() {
+        return 0;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
     }
 }
--- test/TV/TV_Test.java
+++ test/TV/TV_Test.java
@@ -2,14 +2,13 @@ package TV;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TV_Test {
     @Test
     void testThatTvNameIsSet() {
         TV newTV;
-        newTV = new TV();
+        newTV = new TV(0,1);
         assertEquals("Ehizman", newTV.getName());
     }
 
@@ -17,11 +16,73 @@ public class TV_Test {
     void testThatTvCanPowerOn() {
         TV newTV;
 
-        newTV = new TV();
+        newTV = new TV(0,1);
         assertEquals("Ehizman", newTV.getName());
         newTV.setOn(true);
 
         assertTrue(newTV.isOn());
-
     }
+
+    @Test
+    void testThatTvCanPowerOff() {
+        TV newTV;
+
+        newTV = new TV(0,1);
+        newTV.setOn(true);
+        assertTrue(newTV.isOn());
+
+        newTV.setOn(false);
+        assertFalse(newTV.isOn());
+    }
+
+    @Test
+    void testThatTvCanMoveToNextChannel() {
+        TV newTV;
+
+        newTV = new TV(0,1);
+        newTV.setOn(true);
+        newTV.moveToNextChannel();
+
+        assertEquals(2, newTV.getCurrentChannel());
+    }
+
+    @Test
+    void testThatTvChannelIsSetToDefault_1AtCreation() {
+        TV newTV;
+        newTV = new TV(0,1);
+        assertEquals(1, newTV.getCurrentChannel());
+    }
+
+    @Test
+    void testThatTvVolumeIsSetToDefault_0AtCreation() {
+        TV newTV;
+        newTV = new TV(0,1);
+        assertEquals(0, newTV.getCurrentVolume());
+    }
+
+    @Test
+    void testThatTvCanMoveToNextChannelMultipleTimes() {
+        TV newTV;
+
+        newTV = new TV(0,1);
+        assertEquals(0, newTV.getCurrentVolume());
+        assertEquals(1, newTV.getCurrentChannel());
+        newTV.setOn(true);
+        newTV.moveToNextChannel();
+        newTV.moveToNextChannel();
+
+        assertEquals(3, newTV.getCurrentChannel());
+    }
+
+    @Test
+    void testThatTvCanSetChannel() {
+        TV newTV;
+
+        newTV = new TV(0,1);
+        newTV.setChannel(10);
+        assertEquals(10, newTV.getCurrentChannel());
+    }
+
+//    @Test
+//    void testThatTvCannotSetChannelWhenOff)(){}
 }
