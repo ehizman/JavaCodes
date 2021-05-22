008b3e5cf920d1f601edab6eb83e895f7e662d8a
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-22 20:27:19 +0100

    added arithmetic app for chapter two

--- src/chapterTwo/BodyMassIndexCalculatorApp/ArithmeticApp.java
+++ src/chapterTwo/BodyMassIndexCalculatorApp/ArithmeticApp.java
@@ -1,15 +1,21 @@
 package chapterTwo.BodyMassIndexCalculatorApp;
 
 import java.util.Scanner;
-
+/*
+*   Algorithm:
+*                  - Prompt user to enter two numbers
+*                  - Obtain numbers from the user
+*                  - Display sum of numbers
+*                  - Display difference between numbers
+*                   - Display product of numbers
+*                   - Display quotient of division of numbers
+*  */
 public class ArithmeticApp {
 
     public static void main (String ... args) {
         
         int firstNumber;
         int secondNumber;
-        int maximum;
-        int minimum;
         Scanner input = new Scanner(System.in);
         
         System.out.print("Enter first number: ");
@@ -20,13 +26,13 @@ public class ArithmeticApp {
         secondNumber = input.nextInt();
         System.out.println();
 
-        maximum = (firstNumber > secondNumber) ? (firstNumber) : (secondNumber);
-        minimum = (firstNumber < secondNumber) ? (firstNumber) : (secondNumber);
-        
+
         System.out.printf("The sum of %d and %d is: %d%n", firstNumber, secondNumber, (firstNumber + secondNumber));
         System.out.printf("The product of %d and %d is %d%n", firstNumber, secondNumber, (firstNumber * secondNumber));
-        System.out.printf("The difference between %d and %d is: %d%n", firstNumber, secondNumber, (maximum - minimum));
-        System.out.printf("The quotient division of %d divided by %d is: %d%n", maximum, minimum, (maximum/minimum));
+        System.out.printf("The difference between %d and %d is: %d%n", firstNumber, secondNumber,
+                (firstNumber - secondNumber));
+        System.out.printf("The quotient division of %d divided by %d is: %d%n", firstNumber,
+                secondNumber,(firstNumber/secondNumber));
     }
 
 }
