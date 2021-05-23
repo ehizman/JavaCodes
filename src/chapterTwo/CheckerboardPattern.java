e271ebb3d4b26fb82a4daeaa5138353740eca296
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-05-23 16:50:16 +0100

    added checkerboard pattern printer

---
+++ src/chapterTwo/CheckerboardPattern.java
@@ -0,0 +1,11 @@
+package chapterTwo;
+
+public class CheckerboardPattern {
+
+    public static void main(String[] args) {
+        for (int counter = 1; counter < 5; counter++) {
+            System.out.printf("%s%n", "*  *  *  *  *  *  *  *");
+            System.out.printf("%s%n", "  *  *  *  *  *  *  *  *  *");
+        }
+    }
+}
