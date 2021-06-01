3c8015b4300111036830c129e8e05ebfd87f7618
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-06-01 14:52:29 +0100

    corrected logic in code

--- src/assignments/FindingRootsOfQuadraticEquation.java
+++ src/assignments/FindingRootsOfQuadraticEquation.java
@@ -1,7 +1,10 @@
 package assignments;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FindingRootsOfQuadraticEquation {
-    public double[] findRoots(int coefficientOf_A, int coefficientOf_B, int coefficientOf_C) {
+    public static double[] findRoots(int coefficientOf_A, int coefficientOf_B, int coefficientOf_C) {
         double determinantOfEquation = Math.pow(coefficientOf_B, 2.0) - (4 * coefficientOf_A * coefficientOf_C);
 
         if (determinantOfEquation < 0.0){
@@ -11,14 +14,32 @@ public class FindingRootsOfQuadraticEquation {
 
         else{
             double numeratorEquationForFirstRoot = -(coefficientOf_B) - Math.sqrt(determinantOfEquation);
-            double firstRoot = numeratorEquationForFirstRoot/ (2 * coefficientOf_A * coefficientOf_C);
-            firstRoot = Math.round(firstRoot * 1000.0)/ 1000.0;
+            double firstRoot = numeratorEquationForFirstRoot/ (2 * coefficientOf_A);
+            double firstRootToThreeDecimalPlaces = Math.round(firstRoot * 1000.0)/ 1000.0;
 
             double numeratorEquationForSecondRoot = -(coefficientOf_B) + Math.sqrt(determinantOfEquation);
-            double secondRoot = numeratorEquationForSecondRoot/ (2 * coefficientOf_A * coefficientOf_C);
-            secondRoot = Math.round(secondRoot * 1000.0)/ 1000.0;
-            System.out.printf("The roots of the equations are: %f and %f%n", firstRoot, secondRoot);
-            return (new double[] {firstRoot, secondRoot});
+            double secondRoot = numeratorEquationForSecondRoot/ (2 * coefficientOf_A);
+            double secondRootToThreeDecimalPlaces = Math.round(secondRoot * 1000.0)/ 1000.0;
+            return (new double[] {firstRootToThreeDecimalPlaces, secondRootToThreeDecimalPlaces});
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to my Quadratic Equation App");
+
+        System.out.print("Enter the coefficient A: ");
+        int coefficient_A = scanner.nextInt();
+
+        System.out.print("Enter the coefficient B: ");
+        int coefficient_B = scanner.nextInt();
+
+        System.out.print("Enter the coefficient C: ");
+        int coefficient_C = scanner.nextInt();
+        System.out.printf("The roots of the equations are: %s%n", Arrays.toString(findRoots(coefficient_A, coefficient_B,
+                coefficient_C)));
+
+
     }
 }
--- test/assignment/FindingRootsOfQuadraticEquationTest.java
+++ test/assignment/FindingRootsOfQuadraticEquationTest.java
@@ -10,12 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FindingRootsOfQuadraticEquationTest {
     @Test
     void testThatRootsCanBeFoundGivenTheCoefficientsOfTheTermsInAQuadraticEquation(){
-        FindingRootsOfQuadraticEquation calculator = new FindingRootsOfQuadraticEquation();
-        final double division =Math.round(-(1.0/ 6.0) * 1000.0)/1000.0;
+        final double division =Math.round((3.0/ 2.0) * 1000.0)/1000.0;
 
-        assertEquals(new double[]{division, division}[0], calculator.findRoots(-4, +12, -9)[0]);
+        assertEquals(new double[]{division, division}[0], FindingRootsOfQuadraticEquation.findRoots(-4, +12, -9)[0]);
 
-        assertEquals(new double[]{division, division}[1], calculator.findRoots(-4, +12, -9)[1]);
+        assertEquals(new double[]{division, division}[1], FindingRootsOfQuadraticEquation.findRoots(-4, +12, -9)[1]);
 
     }
 }
