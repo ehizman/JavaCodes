5ece98dc1841cf8640dbbfd3b3bb28de029c1ac7
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-06-24 08:44:42 +0100

    implemented account withdrawal and deposit methods

--- src/bankApplication/Account.java
+++ src/bankApplication/Account.java
@@ -1,11 +1,15 @@
 package bankApplication;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.InvalidParameterException;
+
 public class Account {
     private String accountNumber;
     private int accountBalance;
 
     public Account() {
-        accountNumber = Bank.getCustomers().size() +"";
+        accountNumber = Bank.getCustomers().size()+ 1 + "";
         accountBalance = 0;
     }
 
@@ -17,4 +21,26 @@ public class Account {
         return accountBalance;
     }
 
+    public void deposit(int depositAmount) {
+        if (depositAmount < 0){
+            throw new InvalidParameterException("Invalid deposit Amount");
+        }
+        if (Double.parseDouble(depositAmount + "") < 0 || Double.parseDouble(depositAmount + "") > 100000000.00){
+            throw new InvalidParameterException("Invalid deposit Amount");
+        }
+        if(depositAmount == 0){
+            throw new NullPointerException("zero amount to deposit!");
+        }
+        this.accountBalance = this.accountBalance +  depositAmount;
+    }
+
+    public void withdraw(int amountToWithDraw) {
+        if (amountToWithDraw > accountBalance){
+            throw new InvalidParameterException("Invalid withdrawal amount");
+        }
+        if (amountToWithDraw ==0){
+            throw new NullPointerException("You want to withdraw 0");
+        }
+        accountBalance = accountBalance - amountToWithDraw;
+    }
 }
--- test/bankApplication/AccountTest.java
+++ test/bankApplication/AccountTest.java
@@ -2,8 +2,69 @@ package bankApplication;
 
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    @Test
+    void testThatCanDepositToAccount(){
+        Account account = new Account();
+        account.deposit(5000);
+        assertEquals(5000, account.getAccountBalance());
+    }
 
+    @Test
+    void testThatCanWithdraw(){
+        Account account = new Account();
+        account.deposit(5000);
+        account.withdraw(2000);
+        assertEquals(3000, account.getAccountBalance());
+    }
+
+    @Test
+    void testThatCannotDepositAnAmountLessThanZero(){
+        Account account = new Account();
+        assertAll(
+                ()->assertThrows(InvalidParameterException.class, ()->account.deposit(-5000)),
+                ()-> assertEquals(0, account.getAccountBalance())
+        );
+    }
+
+    @Test
+    void testThatCannotDepositAnInvalidAmount(){
+        Account account = new Account();
+        assertAll(
+                ()-> assertThrows(NullPointerException.class, ()-> account.deposit(0)),
+                ()-> assertEquals(0, account.getAccountBalance())
+        );
+    }
+
+    @Test
+    void testThatCanWithDrawFromAccount(){
+        Account account = new Account();
+        account.deposit(5000);
+        account.withdraw(2000);
+        assertEquals(3000, account.getAccountBalance());
+    }
+
+    @Test
+    void testCannotWithDrawAmountGreaterThanBalance(){
+        Account account = new Account();
+        account.deposit(5000);
+        assertAll(
+                ()-> assertThrows(InvalidParameterException.class,()->account.withdraw(50000)),
+                ()-> assertEquals(5000, account.getAccountBalance())
+        );
+    }
+
+    @Test
+    void testCannotWithDrawZeroAmount(){
+        Account account = new Account();
+        account.deposit(5000);
+        assertAll(
+                ()-> assertThrows(NullPointerException.class,()->account.withdraw(0)),
+                ()-> assertEquals(5000, account.getAccountBalance())
+        );
+    }
 }
