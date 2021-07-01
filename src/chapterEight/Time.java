a7ffc4e0c2425000acd74196236c64833b6ca77d
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-07-01 02:30:33 +0100

    added time class with oveerloaded constructor

--- src/chapterEight/Time.java
+++ src/chapterEight/Time.java
@@ -1,26 +1,63 @@
 package chapterEight;
-
-import java.util.NoSuchElementException;
+import java.security.InvalidParameterException;
 
 public class Time {
     int hour;
     int minute;
     int second;
 
-    public void setTime(int hour, int minute, int second) {
-        boolean isInvalid;
-        isInvalid = hour < 0 || hour > 23 || minute < 0 || minute > 59 || second < 0 || second > 59;
-        if (isInvalid){
-            throw new NoSuchElementException("Invalid time!");
+    public Time(int hour){
+        this(hour,0,0);
+    }
+
+    public Time(int hour, int minute){
+        this(hour, minute, 0);
+    }
+
+    public Time (int hour, int minute, int second){
+        try{
+            boolean isValid = validation(hour,minute,second);
+            if (isValid){
+                this.hour = hour;
+                this.minute = minute;
+                this.second = second;
+            }
         }
-        else{
-            this.hour = hour;
-            this.minute = minute;
-            this.second = second;
+        catch (InvalidParameterException error){
+            System.out.println(error.getMessage());
         }
+
+    }
+
+    public static boolean validation(int hour, int minute, int second) {
+        boolean isValid;
+        isValid = hour >= 0 && hour < 24 && minute >= 0 && minute < 60 && second >= 0 && second < 60;
+        if (isValid){
+            return true;
+        }
+        throw new InvalidParameterException("Invalid time parameters");
     }
 
     public String displayUniversalTime() {
         return String.format("%02d:%02d:%02d",hour,minute,second);
+    }
+
+    public String displayTwelveHourTime() {
+        if (hour > 12){
+            return String.format("%02d:%02d:%02d%s",hour - 12,minute,second, "PM");
+        }
+        else{
+            if (hour == 12){
+                return String.format("%02d:%02d:%02d%s",hour,minute,second, "PM");
+            }
+            else{
+                if (hour == 0){
+                    return String.format("%02d:%02d:%02d%s",12,minute,second, "AM");
+                }
+                else {
+                    return String.format("%02d:%02d:%02d%s",hour,minute,second,"AM");
+                }
+            }
+        }
     }
 }
--- test/TimeTest.java
+++ test/TimeTest.java
@@ -1,6 +1,7 @@
 import chapterEight.Time;
 import org.junit.jupiter.api.Test;
 
+import java.security.InvalidParameterException;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,21 +11,30 @@ public class TimeTest {
 
     @Test
     void setTime(){
-        Time clock = new Time();
-        clock.setTime(12,4,56);
+        Time clock = new Time(12,4,56);
         assertEquals("12:04:56", clock.displayUniversalTime());
     }
 
     @Test
     void throwsExceptionWhenInvalidInputIsEntered(){
-        Time clock = new Time();
-        assertThrows(NoSuchElementException.class, ()-> clock.setTime(25,61,56));
+        assertThrows(InvalidParameterException.class, ()-> Time.validation(25,61,56));
     }
 
     @Test
     void displaysTimeInAM_PM_Format(){
-        Time clock = new Time();
-        clock.setTime(22,4,56);
-        assertEquals("10:04:56PM", clock.displayUniversalTime());
+        Time clock = new Time(22,4,56);
+        assertEquals("10:04:56PM", clock.displayTwelveHourTime());
+    }
+
+    @Test
+    void createsConstructor_With_Only_Hour_Parameter(){
+        Time clock = new Time(0);
+        assertEquals("12:00:00AM", clock.displayTwelveHourTime());
+    }
+
+    @Test
+    void createsConstructor_With_Only_Hour_Parameter_And_Minute_Parameter(){
+        Time clock = new Time(12, 23);
+        assertEquals("12:23:00PM", clock.displayTwelveHourTime());
     }
 }
